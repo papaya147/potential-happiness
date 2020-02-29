c768b9d1279c9fb4e62a43d99c64b1859e28a51e
abhinav <abhinav@hs.in>
2020-02-29 16:27:58 +0530

    started view article

---
+++ view-article/Components/Article.js
@@ -0,0 +1,13 @@
+class Article extends React.Component {
+  render() {
+      style={
+        backgroundImage: 
+        (
+          rgba(0, 0, 0, 0.5),
+          rgba(0, 0, 0, 0.5)
+        ),
+        url(this.props.imgURL);
+      }
+    return <div></div>;
+  }
+}
